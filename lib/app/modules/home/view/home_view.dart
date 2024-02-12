f0e98b6706b14c959f3a498f71e63cfb684d01b8
eun-chae-s <s9710173@naver.com>
2024-02-11 23:07:45 -0500

    finally placing the text to the left

--- lib/app/modules/home/view/home_view.dart
+++ lib/app/modules/home/view/home_view.dart
@@ -5,29 +5,40 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Haru';
-    const String subTitle = 'keep track of your day';
+    const String appTitle = 'HARU:';
+    const String subTitle = '하루하루 의미있는 배움의 시간';
+    const String patent = '@rachae';
     const Color backgroundColor = Color(0xFF7B91BC);
-    return SizedBox.expand(
-        child: Container(
-      color: backgroundColor,
-      child: const Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(appTitle,
-                textDirection: TextDirection.ltr,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                )),
-            Text(
-              subTitle,
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.left,
-            )
-          ]),
-    ));
+    return Container(
+        color: backgroundColor,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: const Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  appTitle,
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  subTitle,
+                  textAlign: TextAlign.left,
+                  textDirection: TextDirection.ltr,
+                ),
+                SizedBox(
+                  height: 450,
+                ),
+                Text(
+                  patent,
+                  textDirection: TextDirection.ltr,
+                  textAlign: TextAlign.center,
+                )
+              ],
+            )));
   }
 }
